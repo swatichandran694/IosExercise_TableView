0b062cdcb38d66b3a112c5f0a239fc915e06803e
swatichandran694 <swatichandran694.gmail.com>
2020-01-09 14:42:22 +0530

    Added UI controls to display data in tableview controller

--- IosExercise_TableView/Tableview Cell/DetailsTableViewCell.swift
+++ IosExercise_TableView/Tableview Cell/DetailsTableViewCell.swift
@@ -10,15 +10,84 @@ import UIKit
 
 class DetailsTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+
+    
+    let containerView:UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .clear
+        view.clipsToBounds = true // this will make sure its children do not go out of the boundary
+        //view.backgroundColor = .lightGray
+        return view
+    }()
+    
+    let detailsImageView:UIImageView = {
+        let img = UIImageView()
+        img.contentMode = .scaleAspectFit
+        img.translatesAutoresizingMaskIntoConstraints = false // enable autolayout
+        img.layer.cornerRadius = img.frame.size.width / 2
+        img.layer.shadowRadius = 5;
+        img.layer.shadowOpacity = 0.5;
+        img.clipsToBounds = true
+        return img
+    }()
+    
+    let nameLabel:UILabel = {
+        let label = UILabel()
+        label.font = UIFont.boldSystemFont(ofSize: 17)
+        label.textColor = .black
+        label.backgroundColor = .clear
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let detailedLabel:UILabel = {
+        let label = UILabel()
+        label.textColor =  .darkGray
+        label.backgroundColor = .clear
+        label.numberOfLines = 0
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.contentMode = .scaleToFill
+        label.textAlignment = .left
+        label.lineBreakMode = .byTruncatingTail
+        return label
+    }()
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        self.contentView.addSubview(detailsImageView)
+        containerView.addSubview(nameLabel)
+        containerView.addSubview(detailedLabel)
+        self.contentView.addSubview(containerView)
+        
+        detailsImageView.centerYAnchor.constraint(equalTo:self.contentView.centerYAnchor).isActive = true
+        detailsImageView.leadingAnchor.constraint(equalTo:self.contentView.leadingAnchor, constant:5).isActive = true
+        detailsImageView.widthAnchor.constraint(equalToConstant:100).isActive = true
+        detailsImageView.heightAnchor.constraint(equalToConstant:100).isActive = true
+        
+        containerView.topAnchor.constraint(equalTo:self.contentView.topAnchor).isActive = true
+        containerView.bottomAnchor.constraint(equalTo:self.contentView.bottomAnchor).isActive = true
+        containerView.leadingAnchor.constraint(equalTo:self.detailsImageView.trailingAnchor, constant:10).isActive = true
+        containerView.trailingAnchor.constraint(equalTo:self.contentView.trailingAnchor, constant:-5).isActive = true
+        //containerView.heightAnchor.constraint(equalToConstant:40).isActive = true
+        
+        nameLabel.topAnchor.constraint(equalTo:self.containerView.topAnchor, constant:10).isActive = true
+        nameLabel.leadingAnchor.constraint(equalTo:self.containerView.leadingAnchor, constant:5).isActive = true
+        nameLabel.trailingAnchor.constraint(equalTo:self.containerView.trailingAnchor).isActive = true
+        
+        detailedLabel.topAnchor.constraint(equalTo:self.nameLabel.bottomAnchor).isActive = true
+        detailedLabel.leadingAnchor.constraint(equalTo:self.containerView.leadingAnchor, constant:5).isActive = true
+        detailedLabel.bottomAnchor.constraint(equalTo:self.containerView.bottomAnchor, constant:-5).isActive = true
+        detailedLabel.trailingAnchor.constraint(equalTo:self.containerView.trailingAnchor).isActive = true
+        detailedLabel.heightAnchor.constraint(greaterThanOrEqualToConstant: 50).isActive = true
+        
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder aDecoder: NSCoder) {
+        
+        super.init(coder: aDecoder)
     }
 
 }
