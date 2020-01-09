6909cd9b9b621cf800bb30fe3d998391aa58edf5
swatichandran694 <swatichandran694.gmail.com>
2020-01-09 11:38:11 +0530

    Added Tableview cell Added constraints for tableview

---
+++ IosExercise_TableView/Tableview Cell/DetailsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DetailsTableViewCell.swift
+//  IosAssignment
+//
+//  Created by Swati Chandran on 09/01/20.
+//  Copyright © 2020 SwatiChandran. All rights reserved.
+//
+
+import UIKit
+
+class DetailsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
