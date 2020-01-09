6fd8259a63cc54c05936f2515771248e58bca93c
Swati <swatichandran694@gmail.com>
2020-01-09 11:31:26 +0530

    Initial Commit

---
+++ IosExercise_TableViewTests/IosExercise_TableViewTests.swift
@@ -0,0 +1,36 @@
+//
+//  IosExercise_TableViewTests.swift
+//  IosExercise_TableViewTests
+//
+//  Created by Swati Chandran on 09/01/20.
+//  Copyright © 2020 SwatiChandran. All rights reserved.
+//
+
+import XCTest
+@testable import IosExercise_TableView
+
+class IosExercise_TableViewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
