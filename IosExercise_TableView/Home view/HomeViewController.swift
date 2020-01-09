a2e10415b703f2cfd267e83ada5c4bf26688122e
swatichandran694 <swatichandran694.gmail.com>
2020-01-09 18:52:34 +0530

    Implemented Data viewmodel and mapped data fields to the UITableview

--- IosExercise_TableView/Home view/HomeViewController.swift
+++ IosExercise_TableView/Home view/HomeViewController.swift
@@ -12,17 +12,19 @@ class HomeViewController: UIViewController {
 
     let tableView = UITableView()
     var safeArea: UILayoutGuide!
-     var refreshControl = UIRefreshControl()
+    var refreshControl = UIRefreshControl()
+    var viewModel: DetailsViewModelProtocol = DetailsViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        navigationItem.title = viewModel.navigationTitle
         setupUI()
     }
     
     func setupUI() {
         view.backgroundColor = .green
         safeArea = view.layoutMarginsGuide
+        viewModel.model = DetailsAPI()
         setupTableView()
         
     }
@@ -50,17 +52,25 @@ class HomeViewController: UIViewController {
         }
         
         func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return 10
+            return viewModel.numberOfRows()
         }
         func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+            let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)  as! DetailsTableViewCell
             cell.selectionStyle = .none
-            cell.textLabel?.text = "test data"
+            cell.details = viewModel.itemAtIndex(index: indexPath.row)
             return cell
         }
         
         func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
             return UITableViewAutomaticDimension
+        }
+        
+        func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+            if(indexPath.row % 2 == 0) {
+                cell.backgroundColor = UIColor.white
+            } else {
+                cell.backgroundColor =  UIColor(red: 245.0/255, green: 245.0/255, blue: 245.0/255, alpha: 1.0)
+            }
         }
         
         @objc func refreshData(refreshControl: UIRefreshControl) {
