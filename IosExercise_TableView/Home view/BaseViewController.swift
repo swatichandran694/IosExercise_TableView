e9cd0e5a93761feeb5beb7bbac1d9146c511b906
swatichandran694 <swatichandran694.gmail.com>
2020-01-16 13:13:55 +0530

    Created new files for network api layer

---
+++ IosExercise_TableView/Home view/BaseViewController.swift
@@ -0,0 +1,9 @@
+//
+//  BaseViewController.swift
+//  TableViewExercise
+//
+//  Created by Swati Chandran on 16/01/20.
+//  Copyright © 2020 SwatiChandran. All rights reserved.
+//
+
+import Foundation
